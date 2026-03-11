caa3228c91bebbb638c63fc6a1098986eb71da60
conorbot <cwoodhayes@gmail.com>
2026-03-11 15:22:43 -0500

    drop in of pkg create for polyumi_ros

---
+++ ros2_ws/src/polyumi_ros/setup.py
@@ -0,0 +1,32 @@
+"""Setup for the polyumi_ros package."""
+
+from setuptools import find_packages, setup
+
+package_name = 'polyumi_ros'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        (
+            'share/ament_index/resource_index/packages',
+            ['resource/' + package_name],
+        ),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='conorbot',
+    maintainer_email='cwoodhayes@gmail.com',
+    description='Core ROS2 python nodes for the PolyUMI multimodal learning platform',
+    license='MIT',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [],
+    },
+)
