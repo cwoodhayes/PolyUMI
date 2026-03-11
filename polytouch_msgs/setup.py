7a395d2d4cbb3dde827ebec97a63d2b3d9ddc0c5
conorbot <cwoodhayes@gmail.com>
2026-03-11 15:05:03 -0500

    added protobuf compilation to setup.py

--- polytouch_msgs/setup.py
+++ polytouch_msgs/setup.py
@@ -1,10 +1,29 @@
+"""Setup for the polytouch_msgs package."""
+
+from grpc_tools import protoc
 from setuptools import find_packages, setup
 
 package_name = 'polytouch_msgs'
 
+
+def compile_protos():
+    """Compile the protobuf files."""
+    proto_file = 'polyumi_msgs/camera_frame.proto'
+    protoc.main(
+        [
+            'grpc_tools.protoc',
+            '-I=polyumi_msgs',
+            '--python_out=polyumi_msgs',
+            proto_file,
+        ]
+    )
+
+
+compile_protos()
+
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
     packages=find_packages(exclude=['test']),
     data_files=[
         (
