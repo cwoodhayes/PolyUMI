4d0c344aed6fcd45f7cf83acae6eeead3c99352e
conorbot <cwoodhayes@gmail.com>
2026-05-20 12:57:08 -0500

    fixed esync pin number to avoid collision

--- pi/polyumi_pi/constants.py
+++ pi/polyumi_pi/constants.py
@@ -3,7 +3,7 @@
 # GPIO pin numbers (BCM numbering)
 BUTTON_PIN = 23
 INDICATOR_PIN = 25
-ESYNC_PIN = 21
+ESYNC_PIN = 26  # rpi header pin 37. connect gnd to pin 39 right next door.
 
 # Audio hardware
 AUDIO_DEVICE = 'wm8960-soundcard'
